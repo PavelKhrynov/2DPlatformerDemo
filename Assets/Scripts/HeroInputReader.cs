cd0856e61de924d77f2063beaf582fe83477f636
PavelKhrynov <DarkPreacher@live.ru>
2022-03-27 14:54:07 +0400

    Assets and Input
    - pixel per unit for all assets
    - basic input system enabled

---
+++ Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+
+    private HeroInput _inputAction;
+
+    private void Awake()
+    {
+        _inputAction = new HeroInput();
+        _inputAction.Hero.HorizontalMovement.performed += OnHorizontalMovement;
+        _inputAction.Hero.HorizontalMovement.canceled += OnHorizontalMovement;
+    }
+
+    private void OnEnable()
+    {
+        _inputAction.Enable();
+    }
+
+    public void OnHorizontalMovement(InputAction.CallbackContext context)
+    {
+        var _direction = context.ReadValue<float>();
+        _hero?.SetDirection(_direction);
+    }
+}
