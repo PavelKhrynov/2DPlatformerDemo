18d3566a033600dc007138af7d7811eab297fa4a
PavelKhrynov <DarkPreacher@live.ru>
2022-04-05 22:42:07 +0400

    Basic animations
    - animations throught animator and scripts
    - some prefabs

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -13,10 +13,18 @@ namespace WhereIAm.Scripts
 
         private Rigidbody2D _rigidbody;
         private Vector2 _direction;
+        private Animator _animator;
+        private SpriteRenderer _sprite;
+
+        private static readonly int IsGroundKey = Animator.StringToHash("is-ground");
+        private static readonly int IsRunningKey = Animator.StringToHash("is-running");
+        private static readonly int VerticalVelocityKey = Animator.StringToHash("vertical-velocity");
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _sprite = GetComponent<SpriteRenderer>();
         }
 
         public void SetDirection(Vector2 direction)
@@ -29,9 +37,11 @@ namespace WhereIAm.Scripts
             _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
 
             var isJumping = _direction.y > 0;
+            var isGrounded = IsGrounded();
+
             if (isJumping)
             {
-                if (IsGrounded())
+                if (isGrounded)
                 {
                     _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
                 }
@@ -41,6 +51,23 @@ namespace WhereIAm.Scripts
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
             }
 
+            _animator.SetFloat(VerticalVelocityKey, _rigidbody.velocity.y);
+            _animator.SetBool(IsRunningKey, _direction.x != 0);
+            _animator.SetBool(IsGroundKey, isGrounded);
+
+            UpdateSpriteDirection();
+        }
+
+        private void UpdateSpriteDirection()
+        {
+            if (_direction.x > 0)
+            {
+                _sprite.flipX = false;
+            }
+            else if (_direction.x < 0)
+            {
+                _sprite.flipX = true;
+            }
         }
 
         private bool IsGrounded()
