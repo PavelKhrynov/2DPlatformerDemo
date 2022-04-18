e272cf7b50c6ae484f7490af0b11b4bc25b275f1
PavelKhrynov <DarkPreacher@live.ru>
2022-04-18 21:45:21 +0400

    Teleport Component

---
+++ Assets/Scripts/Component/TeleportComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace WhereIAm.Scripts.Component
+{
+    public class TeleportComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _destination;
+
+        public void Teleport(GameObject target)
+        {
+            target.transform.position = _destination.position;
+        }
+    }
+}
